6180d4af6913ab999ce7678fe0755a6ea5bd54cb
fbukevin <fbukevin@gmail.com>
2016-11-14 21:11:52 +0800

    do many things that i cannot add message

--- register.js
+++ register.js
@@ -1,5 +1,9 @@
 
 function checkForm(){
+
+	$('#submit').hide()
+	$('#loading').show()
+
 	var uid = $('#uid')[0].value
 	var pwd1 = $('#pwd1')[0].value
 	var pwd2 = $('#pwd2')[0].value
@@ -13,21 +17,37 @@ function checkForm(){
 			if(pwd1 == pwd2){
 				if(realName == ''){
 					alert('姓名不得為空')
-					return false
 				} else {
-					return true
+					$.ajax({
+				    url: 'http://fastpowerdev.herokuapp.com/client/register',
+				    type: "POST",
+				    dataType: "json",
+				    data: { uid: uid, pwd: pwd1, username: realName },
+				    success: function (res) {
+				  		$('#loading').hide()
+							$('#submit').show()				         
+		         	if(res.status == 30){
+								alert('該門號已註冊\n如未驗證手機，請於隔日再重新註冊或洽服務人員')
+		         	} else if(res.status == 29){
+		         		alert('密碼格式錯誤')
+		         	} else if(res.status == 28){
+		         		window.location = "verify.html?q=" + uid
+		         	}
+				    },
+				    error: function (xhr, ajaxOptions, thrownError) {
+				         console.log(xhr.status, xhr.responseText);
+				    }
+				 });
+
 				}
 			} else {
-				alert('兩次輸入密碼不相同')
-				return false
+				alert('兩次輸入密碼不相同')				
 			}
 		} else {
-			alert('密碼格式錯誤')
-			return false
+			alert('密碼格式錯誤')		
 		}
 	} else {
 		alert('手機格式錯誤')
-		return false
 	}
 }
 
---
+++ verify.js
@@ -0,0 +1,49 @@
+
+function parseUid(){
+	var parser = document.createElement('a');
+	parser.href = location.href;
+	var search = parser.search.split('?')[1]	// =>['a=1&b=2']
+	var queryString = search.split('&')				// ['a=1', 'b=2', ...]
+	var query = queryString.map((q)=>{ return q.split('=')[1] })
+	return query;
+}
+
+function checkAuthCode(){
+
+	$('#submit').hide()
+	$('#loading').show()
+
+	var code = $('#auth-code')[0].value
+	var uid = parseUid()[0]
+
+	if(code != ''){		
+		$.ajax({
+	    url: 'http://fastpowerdev.herokuapp.com/client/auth_code',
+	    type: "POST",
+	    dataType: "json",
+	    data: { uid: uid, code: code },
+	    success: function (res) {
+				$('#loading').hide()	    	
+	     	$('#submit').show()				
+
+	     	if(res.status == 3){
+					alert('驗證碼錯誤')
+	     	} else if(res.status == 53){
+	     		alert('伺服器更新錯誤')
+	     	} else if(res.status == 52){
+	     		alert('歡迎加入 Fast Power! 新會員可獲得 2 小時時數，請以此帳號登入')
+	     		window.location = "member.html"
+	     	}
+	    },
+	    error: function (xhr, ajaxOptions, thrownError) {
+	         console.log(xhr.status, xhr.responseText);
+	    }
+	 });
+	} else {
+		alert('不得為空')
+	}
+}
+
+function resendSMS(){
+
+}

87094ffa7404754e57dccb21b10d9bf801560099
fbukevin <fbukevin@gmail.com>
2016-11-14 17:05:08 +0800

    update member register

--- member.js
+++ member.js
@@ -1,11 +1,11 @@
 
 $('#agreement').click(()=>{
 
-	var form = $('#register-form')
+	var btn = $('.to-register')
 
-	if(form[0].classList.contains('active') == false)	// result of the first loading is also false
-   	form.slideDown().addClass("active");	// this will remove style="display: none"
-  else if(form[0].classList.contains('active') == true)
-   	form.slideUp().removeClass("active");
+	if(btn[0].classList.contains('active') == false)	// result of the first loading is also false
+   	btn.slideDown().addClass("active");	// this will remove style="display: none"
+  else if(btn[0].classList.contains('active') == true)
+   	btn.slideUp().removeClass("active");
 
 })
